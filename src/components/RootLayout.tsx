6f2c04e8b071c99d2c787fde5224e8f0ac6d9c80
Zeke Abshire <zekeabshire@gmail.com>
2023-04-29 20:31:13 -0500

    Added new font + svgs

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,8 +1,15 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
+import localFont from "next/font/local";
 import { Header } from "~/components/Header";
 import { Footer } from "~/components/Footer";
 import type { PropsWithChildren } from "react";
 
+const roadster = localFont({
+  src: "../../public/fonts/roadsterscript-gopgq.ttf",
+  display: "block",
+  preload: true,
+  variable: "--font-roadster",
+});
 const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
   weight: ["400", "600", "800"],
@@ -15,7 +22,9 @@ export const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
       <Header />
-      <main className={`${jakarta.variable} min-h-screen bg-white font-sans`}>
+      <main
+        className={`${jakarta.variable} ${roadster.variable} min-h-screen bg-white font-sans`}
+      >
         {children}
       </main>
       <Footer />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { RootLayout } from "~/components/RootLayout";
 
 const Home: NextPage = () => {
   return (
@@ -10,13 +9,13 @@ const Home: NextPage = () => {
         <meta name="description" content="A software development company." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <RootLayout>
+      <>
         <Hero />
         {/* <Services /> */}
         {/* <Projects /> */}
         {/* <About /> */}
         {/* <Contact /> */}
-      </RootLayout>
+      </>
     </>
   );
 };
@@ -26,7 +25,7 @@ export default Home;
 const Hero = () => {
   return (
     <>
-      <h1 className="font-extrabold text-black">Sunrise</h1>
+      <h1 className="py-3 font-roadster text-5xl text-black">Sunrise</h1>
       <p className="leading-relaxed text-black">
         Creating software that looks and works great is our specialty at
         Sunrise. Our team of experts combines artistry and technical know-how to
