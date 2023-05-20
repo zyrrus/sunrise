f809abcf6d9cfe827f29b5f208662c8110389394
Zeke Abshire <zekeabshire@gmail.com>
2023-05-20 16:54:46 -0500

    SR-009: Created responsive typography plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,14 @@ const Home: NextPage = () => {
       </Head>
       <>
         <Hero />
+        <div className="">
+          <p className="text-r-3xl">48px: h1</p>
+          <p className="text-r-2xl">40px: h2</p>
+          <p className="text-r-xl">32px: h3</p>
+          <p className="text-r-lg">24px: h4, header, polaroid, button</p>
+          <p className="text-r-base">20px: h5, body</p>
+          <p className="text-r-sm">16px: h6</p>
+        </div>
         {/* <Services /> */}
         {/* <Projects /> */}
         {/* <About /> */}
@@ -25,8 +33,8 @@ export default Home;
 const Hero = () => {
   return (
     <>
-      <h1 className="py-3 font-roadster text-5xl text-black">Sunrise</h1>
-      <p className="leading-relaxed text-black">
+      <h1 className="py-3 font-roadster text-black text-r-3xl">Sunrise</h1>
+      <p className="leading-relaxed text-black text-r-xl">
         Creating software that looks and works great is our specialty at
         Sunrise. Our team of experts combines artistry and technical know-how to
         craft solutions that will make your business{" "}
